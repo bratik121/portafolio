cbfd3724f068fac6eeeea761cb67a8bacd2118ff
bratik121 <bratik121@gmail.com>
2023-01-30 21:27:02 -0400

    checkpoint

--- src/components/about/InfoSlider.tsx
+++ src/components/about/InfoSlider.tsx
@@ -1,23 +1,23 @@
 import { aboutList, about } from "../../data/about";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
 
-function InfoSlider() {
-	const containerVariant = {
-		hidden: {
-			opacity: 0,
-			y: 20,
+const containerVariant = {
+	hidden: {
+		opacity: 0,
+		y: 20,
+	},
+	show: {
+		opacity: 1,
+		y: 0,
+		transition: {
+			duration: 0.5,
+			ease: "easeInOut",
 		},
-		show: {
-			opacity: 1,
-			y: 0,
-			transition: {
-				duration: 0.5,
-				ease: "easeInOut",
-			},
-		},
-	};
+	},
+};
 
+function InfoSlider() {
 	const [index, setIndex] = useState<number>(0);
 	const nextItem = (index: number) => {
 		if (index !== aboutList.length - 1) {
@@ -28,33 +28,37 @@ function InfoSlider() {
 	};
 
 	return (
-		<div className="w-full col-span-2  md:col-span-3 flex flex-col items-center">
-			<motion.div
-				className="w-full hover:cursor-pointer"
-				variants={containerVariant}
-				initial="hidden"
-				animate="show"
-				key={index}
-				onClick={() => {
-					nextItem(index);
-				}}
-			>
-				<h3 className="font-semibold mb-2">{aboutList[index].tittle}</h3>
-				<ul className=" about__p text-gris-500 text-justify dark:text-gris-200 list-disc px-4 w-full">
-					{aboutList[index].content.map((content: string, indexLi: number) => {
-						return (
-							<li
-								key={indexLi}
-								className=" marker:text-rojo-200 dark:marker:text-naranja-200"
-							>
-								<p key={indexLi}>{content}</p>
-							</li>
-						);
-					})}
-				</ul>
-			</motion.div>
+		<div className="w-full col-span-2  md:col-span-3 flex flex-col items-center min-h-[256px] justify-between">
+			<AnimatePresence>
+				<motion.div
+					className="w-full hover:cursor-pointer"
+					variants={containerVariant}
+					initial="hidden"
+					animate="show"
+					key={index}
+					onClick={() => {
+						nextItem(index);
+					}}
+				>
+					<h3 className="font-semibold mb-2">{aboutList[index].tittle}</h3>
+					<ul className=" about__p text-gris-500 text-justify dark:text-gris-200 list-disc px-4 w-full">
+						{aboutList[index].content.map(
+							(content: string, indexLi: number) => {
+								return (
+									<li
+										key={indexLi}
+										className=" marker:text-rojo-200 dark:marker:text-naranja-200"
+									>
+										<p key={indexLi}>{content}</p>
+									</li>
+								);
+							}
+						)}
+					</ul>
+				</motion.div>
+			</AnimatePresence>
 			{/* Slider Controls */}
-			<div className="flex justify-around  w-[20%] mt-4">
+			<div className="flex justify-around  w-[20%] mt-2">
 				{aboutList.map((element: about, indexE: number) => {
 					return (
 						<span
